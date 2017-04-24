7cc612af32a2d54537aa30b198ed139c13c9788c
Francesco Pasqua <cesconix@me.com>
2017-04-24 17:40:39 +0200

    debug on mongoose

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 const config = require('pitch-config')
 
+const util = require('util')
+const debug = require('debug')('pitch-database:index')
+
 function connect () {
   mongoose.connect(config.database.uri, { server: { socketOptions: { keepAlive: 1 } } })
   mongoose.connection.on('error', () => {
     throw new Error(`Unable to connect to database "${config.database.uri}"`)
   })
 }
+
+mongoose.set('debug', (collectionName, method, query, doc) => {
+  debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc)
+})
 
 module.exports = {
   models: require('./models'),
