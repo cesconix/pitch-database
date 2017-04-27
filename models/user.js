91197a2731d1a286e24f289482059152228be44e
Francesco Pasqua <cesconix@me.com>
2017-04-27 18:54:03 +0200

    changes on User Model

--- models/user.js
+++ models/user.js
@@ -2,15 +2,66 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  provider: String,
-  profile: Schema.Types.Mixed,
-  friends: Array,
+  profile: {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true
+    },
+    locale: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    birthday: {
+      type: Date,
+      required: true
+    },
+    interestedIn: {
+      type: String,
+      required: true
+    }
+  },
+  provider: {
+    name: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    friends: []
+  },
   socket: {
-    id: { type: String, required: false },
-    status: { type: Number, default: -1 }
+    id: {
+      type: String,
+      required: false
+    },
+    status: {
+      type: Number,
+      default: -1
+    }
   },
   random: Number,
-  created: { type: Date, default: Date.now }
+  created: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 UserSchema.statics.connect = (userId, socketId) => {
